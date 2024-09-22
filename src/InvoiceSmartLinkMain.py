116eef1c76978bd886bed7d6816e3bea6c676daa
curtis <curtis_han@outlook.com>
2024-09-22 09:18:31 +0900

    使工程标准化

---
+++ src/InvoiceSmartLinkMain.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Filename: InvoiceSmartLinkMain.py
+Description: Main entry point for the Invoice Smart Link application.
+Author: [TangPing.co]
+Date: [2024-09-15]
+Version: [V0.1]
+Dependencies: [T.B.D]
+"""
+"""
+主要功能
+1
+"""
